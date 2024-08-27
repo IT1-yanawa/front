e0b777c79625cf904e1aafe97d4d50942f0856b1
yhj814 <hojungyong1@gmail.com>
2024-08-28 02:45:22 +0900

    register_js updated

--- static/js/match_page/register.js
+++ static/js/match_page/register.js
@@ -12,12 +12,14 @@ document.querySelectorAll(".span-step").forEach((step) => {
         step.style.backgroundColor = "";
     });
 });
+// 다음단계 이전단계 등록완료 버튼 가져오가
 const nextButton1 = document.querySelector(".next-button-1");
 const nextButton2 = document.querySelector(".next-button-2");
 const beforeButton1 = document.querySelector(".before-button-1");
 const beforeButton2 = document.querySelector(".before-button-2");
 const finishButton = document.querySelector(".finish-button");
 
+// 각 폼들 가져오기
 const form1 = document.getElementById("form-1");
 const form2 = document.getElementById("form-2");
 const form3 = document.getElementById("form-3");
@@ -45,7 +47,7 @@ beforeButton2.addEventListener("click", () => {
     form3.style.display = "none";
     form2.style.display = "block";
 });
-
+// 등록완료 버튼 누를시 alert로 알림뜸
 finishButton.addEventListener("click", () => {
     alert("매칭 등록 완료!!");
 });

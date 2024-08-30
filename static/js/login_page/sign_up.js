bc1ef1edf39cff5e2a5fb281e7a8976509496679
000jisoo <000jisoo@gmail.com>
2024-08-30 18:57:40 +0900

    sign_up 40%

--- static/js/login_page/sign_up.js
+++ static/js/login_page/sign_up.js
@@ -1,53 +1,3 @@
-const agreeDivCheckboxes = document.querySelectorAll(".input-style-agree");
-const agreeCheckboxOns = document.querySelectorAll(".agree-checkbox.on");
-const agreeCheckboxOffs = document.querySelectorAll(".agree-checkbox.off");
-const isAgreeAll = document.querySelector("input[name=is_agree_all]");
-
-isAgreeAll.addEventListener("click", (e) => {
-    if (e.target.checked) {
-        agreeCheckboxOns.forEach((agreeCheckboxOn, i) => {
-            agreeCheckboxOn.style.display = "block";
-            agreeCheckboxOffs[i].style.display = "none";
-        });
-    } else {
-        agreeCheckboxOffs.forEach((agreeCheckboxOff, i) => {
-            agreeCheckboxOns[i].style.display = "none";
-            agreeCheckboxOff.style.display = "block";
-        });
-    }
-});
-
-// agreeDivCheckboxes가 NodeList 또는 HTMLCollection일 경우 배열로 변환
-const agreeDivCheckboxesArray = [...agreeDivCheckboxes];
-
-// 첫 번째 요소를 제외한 배열을 만듭니다.
-const filteredCheckboxes = agreeDivCheckboxesArray.slice(1);
-
-filteredCheckboxes.forEach((filteredCheckbox, i) => {
-    filteredCheckbox.addEventListener("click", (e) => {
-        const originalIndex = i + 1;
-        if (e.target.checked) {
-            agreeCheckboxOns[originalIndex].style.display = "block";
-            agreeCheckboxOffs[originalIndex].style.display = "none";
-        } else {
-            agreeCheckboxOns[originalIndex].style.display = "none";
-            agreeCheckboxOffs[originalIndex].style.display = "block";
-        }
-
-        isAgreeAll.checked = filteredCheckboxes.every(
-            (checkbox) => checkbox.checked
-        );
-
-        if (isAgreeAll.checked) {
-            agreeCheckboxOns[0].style.display = "block";
-            agreeCheckboxOffs[0].style.display = "none";
-        } else {
-            agreeCheckboxOns[0].style.display = "none";
-            agreeCheckboxOffs[0].style.display = "block";
-        }
-    });
-});
-
 const agreeCheckIcons = document.querySelectorAll(".input-style-check-icon");
 const checkIconSvgOns = document.querySelectorAll(".check-icon-svg.on");
 const checkIconSvgOffs = document.querySelectorAll(".check-icon-svg.off");
@@ -57,6 +7,11 @@ const isAcceptEventAll = document.querySelector(
 const isAcceptRecruitAll = document.querySelector(
     "input[name=is_accept_recruit_all]"
 );
+
+const agreeDivCheckboxes = document.querySelectorAll(".input-style-agree");
+const agreeCheckboxOns = document.querySelectorAll(".agree-checkbox.on");
+const agreeCheckboxOffs = document.querySelectorAll(".agree-checkbox.off");
+const isAgreeAll = document.querySelector("input[name=is_agree_all]");
 
 const agreeCheckIconsArray = [...agreeCheckIcons];
 const firstCheckIcons = agreeCheckIconsArray.slice(0, 3);
@@ -101,26 +56,119 @@ isAcceptRecruitAll.addEventListener("click", (e) => {
 firstCheckIcons.forEach((firstCheckIcon, i) => {
     firstCheckIcon.addEventListener("click", (e) => {
         if (e.target.checked) {
-            filteredCheckboxes[4].target.checked;
             firstCheckIconSvgOns[i].style.display = "block";
             firstCheckIconSvgOffs[i].style.display = "none";
         } else {
             firstCheckIconSvgOns[i].style.display = "none";
             firstCheckIconSvgOffs[i].style.display = "block";
         }
+
+        // 부모 체크박스 상태를 직접 업데이트
+        const allUnchecked = firstCheckIcons.every(
+            (checkbox) => !checkbox.checked
+        );
+
+        if (allUnchecked) {
+            agreeDivCheckboxesArray[4].checked = false;
+            agreeCheckboxOns[4].style.display = "none";
+            agreeCheckboxOffs[4].style.display = "block";
+        } else {
+            agreeDivCheckboxesArray[4].checked = true;
+            agreeCheckboxOns[4].style.display = "block";
+            agreeCheckboxOffs[4].style.display = "none";
+        }
     });
 });
 
-// lastCheckIcons.forEach((lastCheckIcon, i) => {
-//     lastCheckIcon.addEventListener("click", (e) => {
-//         if (e.target.checked) {
-//             filteredCheckboxes[5].target.checked;
-//             lastCheckIconSvgOns[i].style.display = "block";
-//             lastCheckIconSvgOffs[i].style.display = "none";
-//         } else {
-//             lastCheckIconSvgOns[i].style.display = "none";
-//             lastCheckIconSvgOffs[i].style.display = "block";
-//         }
+lastCheckIcons.forEach((lastCheckIcon, i) => {
+    lastCheckIcon.addEventListener("click", (e) => {
+        if (e.target.checked) {
+            lastCheckIconSvgOns[i].style.display = "block";
+            lastCheckIconSvgOffs[i].style.display = "none";
+        } else {
+            lastCheckIconSvgOns[i].style.display = "none";
+            lastCheckIconSvgOffs[i].style.display = "block";
+        }
+
+        const allUnchecked = lastCheckIcons.every(
+            (checkbox) => !checkbox.checked
+        );
+
+        if (allUnchecked) {
+            isAcceptRecruitAll.checked = false;
+            agreeCheckboxOns[5].style.display = "none";
+            agreeCheckboxOffs[5].style.display = "block";
+        } else {
+            isAcceptRecruitAll.checked = true;
+            agreeCheckboxOns[5].style.display = "block";
+            agreeCheckboxOffs[5].style.display = "none";
+        }
+    });
+});
+
+// agreeDivCheckboxes가 NodeList 또는 HTMLCollection일 경우 배열로 변환
+const agreeDivCheckboxesArray = [...agreeDivCheckboxes];
+
+// 첫 번째 요소를 제외한 배열을 만듭니다.
+const filteredCheckboxes = agreeDivCheckboxesArray.slice(1);
+
+filteredCheckboxes.forEach((filteredCheckbox, i) => {
+    filteredCheckbox.addEventListener("click", (e) => {
+        const originalIndex = i + 1;
+        if (e.target.checked) {
+            filteredCheckboxes[i].checked = true;
+            agreeCheckboxOns[originalIndex].style.display = "block";
+            agreeCheckboxOffs[originalIndex].style.display = "none";
+        } else {
+            filteredCheckboxes[i].checked = false;
+            agreeCheckboxOns[originalIndex].style.display = "none";
+            agreeCheckboxOffs[originalIndex].style.display = "block";
+        }
+
+        isAgreeAll.checked = filteredCheckboxes.every(
+            (checkbox) => checkbox.checked
+        );
+
+        if (isAgreeAll.checked) {
+            agreeCheckboxOns[0].style.display = "block";
+            agreeCheckboxOffs[0].style.display = "none";
+        } else {
+            agreeCheckboxOns[0].style.display = "none";
+            agreeCheckboxOffs[0].style.display = "block";
+        }
+    });
+});
+
+isAgreeAll.addEventListener("click", (e) => {
+    if (e.target.checked) {
+        agreeCheckboxOns.forEach((agreeCheckboxOn, i) => {
+            agreeDivCheckboxes[i].checked = true;
+            agreeCheckIcons[i].checked = true;
+            agreeCheckboxOn.style.display = "block";
+            agreeCheckboxOffs[i].style.display = "none";
+            checkIconSvgOns[i].style.display = "block";
+            checkIconSvgOffs[i].style.display = "none";
+        });
+    } else {
+        agreeCheckboxOffs.forEach((agreeCheckboxOff, i) => {
+            agreeDivCheckboxes[i].checked = false;
+            agreeCheckIcons[i].checked = false;
+            agreeCheckboxOns[i].style.display = "none";
+            agreeCheckboxOff.style.display = "block";
+            checkIconSvgOffs[i].style.display = "block";
+            checkIconSvgOns[i].style.display = "none";
+        });
+    }
+});
+
+// console.log(firstCheckIcons);
+
+// 한개를 해제했을 때, 두개를 해제했을 때: 체크 true / ON style
+// 세개(전체)를 해제했을 때: 체크 false / off style
+
+// firstCheckIcons.forEach((firstCheckIcon) => {
+//     firstCheckIcon.addEventListener("click", () => {
+
 //     });
 // });
 
